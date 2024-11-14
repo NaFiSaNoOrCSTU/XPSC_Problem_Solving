e9c42906d6fbf98abd4d40756a09ceae34e08544
NaFiSaNoOrCSTU <nafisa.noor.1609@gmail.com>
2024-11-14 23:09:48 +0600

    Add files via upload
    Week 3 Day 6

---
+++ Day 6/Max_Sum_Subarray_Of_Size_K.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    long long maximumSumSubarray(vector<int>& arr, int k) {
+        // code here
+        int n=arr.size(),l=0,r=0;
+        long long ans=0,sum=0;
+        while(r<n)
+        {
+            sum+=arr[r];
+            if(r-l+1==k)
+            {
+                ans=max(ans,sum);
+                sum-=arr[l];
+                l++;
+                r++;
+            }
+            else
+            {
+                r++;
+            }
+            //or if er por r++ dilei hb
+        }
+        return ans;
+    }
+};
